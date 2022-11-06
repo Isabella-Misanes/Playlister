cc3b406b0db4db2715f17e118b8ab33aa3181118
Isabella-Misanes <99161680+Isabella-Misanes@users.noreply.github.com>
2022-11-06 02:23:24 -0500

    Show Edit and Remove Song Modals

--- client/src/components/MUIEditSongModal.js
+++ client/src/components/MUIEditSongModal.js
@@ -49,7 +49,7 @@ export default function MUIEditSongModal() {
 
     return (
         <Modal
-            open={store.listMarkedForDeletion !== null}
+            open={store.showEditSongModal !== null}
         >
             <Box sx={style}>
             <div
