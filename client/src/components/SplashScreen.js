43a61b56f8b6266e0d4d886134eb919086e7e6f1
Isabella-Misanes <99161680+Isabella-Misanes@users.noreply.github.com>
2022-12-04 19:17:17 -0500

    Completed Splash Screen

--- client/src/components/SplashScreen.js
+++ client/src/components/SplashScreen.js
@@ -1,7 +1,14 @@
+import Button from '@mui/material/Button';
 export default function SplashScreen() {
     return (
         <div id="splash-screen">
-            Playlister
+            <h1>Welcome to</h1><p/>
+            <img src="Playlister.png" alt="Playlister logo"></img>
+            <h3>The #1 site for creating and sharing music playlists all around the world!</h3><p/>
+            <Button variant="contained" href="register">Create Account</Button><p/>
+            <Button variant="contained" href="login">Login</Button><p/>
+            <Button variant="contained" href="">Continue as Guest</Button><p/>
+            Created by Isabella Misanes
         </div>
     )
 }
